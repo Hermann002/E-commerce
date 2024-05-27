68495729b80030a264375caa089a0c83bc2e20e8
Hermann002 <hermannnzeudeu@gmail.com>
2024-05-27 16:11:41 +0100

    final project

--- shop/admin.py
+++ shop/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from .models import Category, Product, Order
 
+admin.site.site_header = "E-commerce"
+admin.site.site_title = "sgbd-shop"
+admin.site.index_title = "Manager"
+
 class AdminCategory(admin.ModelAdmin):
     list_display = ('name', 'date_added')
 
 class AdminProduct(admin.ModelAdmin):
     list_display = ('title', 'price', 'category', 'date_added')
+    search_fields = ('title',)
+    list_editable = ('price',)
 
 class AdminOrder(admin.ModelAdmin):
     list_display = ('items', 'name', 'email', 'address', 'city', 'state', 'order_date')
